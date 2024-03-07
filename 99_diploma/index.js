a45557519962a3fbe438a0c0ba01d94e88f592e7
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-03-07 14:15:54 +0500

    auth

--- 99_diploma/index.js
+++ 99_diploma/index.js
@@ -1,5 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
 const nunjucks = require("nunjucks");
+const { nanoid } = require("nanoid");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+});
 
 const app = express();
 
@@ -9,3 +26,112 @@ nunjucks.configure("views", {
 });
 
 app.set("view engine", "njk");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.set("view engine", "njk");
+
+const hash = (data) => crypto.createHash("sha256").update(data).digest("hex");
+
+const auth = () => async (req, res, next) => {
+  if (!req.cookies["sessionId"]) {
+    return next();
+  }
+  const user = await findUserBySessionId(req.cookies["sessionId"]);
+  req.user = user;
+  req.sessionId = req.cookies["sessionId"];
+  next();
+};
+
+app.get("/", auth(), (req, res) => {
+  res.render("index", {
+    user: req.user,
+    authError: req.query.authError === "true" ? "Wrong username or password" : req.query.authError,
+  });
+});
+
+app.post("/signup", bodyParser.urlencoded({ extended: false }), async (req, res) => {
+  const { username, password } = req.body;
+  await createUser(username, password);
+  res.redirect("/dashboard");
+});
+
+app.post("/login", bodyParser.urlencoded({ extended: false }), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserByUsername(username);
+  if (!user || user.password != hash(password)) {
+    res.sendStatus(401);
+    return;
+  }
+  const sessionId = await createSession(user.id);
+  res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/dashboard");
+});
+
+app.get("/logout", auth(), async (req, res) => {
+  if (!req.user) {
+    return res.redirect("/");
+  }
+  await deleteSession(req.sessionId);
+  res.clearCookie("sessionId").redirect("/");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(404).send(err.message);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`  Listening on http://localhost:${port}`);
+});
+
+const createUser = async (username, password) => {
+  const newUser = await knex("users").insert({
+    username: username,
+    password: hash(password),
+  });
+  return newUser;
+};
+
+const findUserByUsername = async (username) =>
+  knex("users")
+    .select()
+    .where({ username })
+    .limit(1)
+    .then((results) => results[0]);
+
+const findUserBySessionId = async (sessionId) => {
+  const session = await knex("sessions")
+    .select("user_id")
+    .where({ session_id: sessionId })
+    .limit(1)
+    .then((results) => results[0]);
+
+  if (!session) {
+    return;
+  }
+
+  return knex("users")
+    .select()
+    .where({ id: session.user_id })
+    .limit(1)
+    .then((results) => results[0]);
+};
+
+const createSession = async (userId) => {
+  const sessionId = nanoid();
+
+  await knex("sessions").insert({
+    user_id: userId,
+    session_id: sessionId,
+  });
+
+  return sessionId;
+};
+
+const deleteSession = async (sessionId) => {
+  await knex("sessions").where({ session_id: sessionId }).delete();
+};
+
